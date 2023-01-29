05e3681b55a82777512386a537d980d008046815
Maxim McNair <mail@maximmcnair.com>
2023-01-29 12:36:35 -0600

    fix glslcanvas on server render

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,23 @@
   import { onMount } from 'svelte';
   import type { Node, NodesObj } from './types';
   import Nodes from './comps/Nodes.svelte';
-  import Canvas from './comps/Canvas.svelte';
   import nodesStore from './stores/nodes';
 
   let nodes: Node[] = [];
   let nodesObj: NodesObj;
+  let canvas: any = null;
 
-  onMount(() => {
+  onMount(async () => {
     nodesStore.subscribe(({ nodes: nds, nodesObj: ndsObj }) => {
       nodes = nds;
       nodesObj = ndsObj;
     });
+
+    try {
+      canvas = (await import('./comps/Canvas.svelte')).default;
+    } catch (err) {
+      console.error(err);
+    }
   });
 </script>
 
@@ -22,4 +28,5 @@
 </svelte:head>
 
 <Nodes {nodes} {nodesObj} />
-<Canvas {nodes} {nodesObj} />
+
+<svelte:component this={canvas} {nodes} {nodesObj} />
